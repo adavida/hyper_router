4486c7561f627a3736fbdaf033bc8385a1d0beee
David ADLER <david.adler@outlook.com>
2023-11-05 21:01:22 +0100

    feat: add default valut on sample

--- simple_example/src/controller.rs
+++ simple_example/src/controller.rs
@@ -3,12 +3,18 @@ use serde::{Deserialize, Serialize};
 #[derive(Debug, PartialEq, Deserialize, Serialize)]
 pub struct EchoParm {
     param1: String,
-    param2: String,
+    param2: Option<String>,
+    #[serde(default = "default_param3")]
+    param3: String,
 }
+fn default_param3() -> String {
+    "param3".to_string()
+}
+
 pub fn hello_world() -> String {
     "Hello, World".to_string()
 }
 
 pub fn echo(entry: &str, pv: EchoParm) -> String {
-    format!("{} ,  {:?}", entry.to_string(), pv)
+    format!("{} ,  {:?}", entry, pv)
 }
